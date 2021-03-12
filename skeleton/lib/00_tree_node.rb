70e674ec9d4dd1442ff7e015a86ae224c88a3d63
Sebastian Sanchez <sebastian@artxcode.io>
2021-03-12 13:59:41 -0500

    pushed skeleton files to start

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,16 @@
+class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value=0)
+        @value = value
+        @parent = nil
+        @children = []
+    end 
+
+    def parent=(node)
+        @parent = node 
+        self.children << 
+    end 
+
+end
