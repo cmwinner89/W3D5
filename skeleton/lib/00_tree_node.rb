9848611f23e18b43831cccbc78181d4fee94ba60
Collin Winner <cmwinner89@gmail.com>
2021-03-12 17:15:54 -0500

    Solve Phase 1

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -12,15 +12,46 @@ class PolyTreeNode
     end 
 
     def parent=(node)
+        # node.parent.children.delete(node)
+        return if self.parent == node
+        if self.parent
+            self.parent.children.delete(self)
+        end
         @parent = node 
-        @parent.children << self
+
+        self.parent.children << self unless self.parent.nil?
+        # if !@parent.children.include?(self)
+        #     @parent.children << self
+        # end
+        # if !@parent.children.empty?
+        #     @parent.children.delete(self)
+        # end\
+        # 
+        # @parent.children.shift
+        self
     end 
 
     def add_child(child)
         child.parent = self
     end
 
-    def remove_child(node)
-        node.parent = self 
+    def remove_child(child)
+        if child && !self.children.include?(child)
+            raise "Tried to remove node that isn't a child"
+          end
+        child.parent = nil 
+        
+        # raise "Error" if self.parent == node.parent
+           
+    end
+
+    def inspect
+        {   
+            value: @value,
+            parent: @parent.value,
+            children: @children.map {|ele| ele.value}
+        }.to_s
+        # [value, children]
+
     end
 end
