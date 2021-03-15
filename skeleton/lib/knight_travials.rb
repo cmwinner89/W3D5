3279f57af3fcd1a69e655f884ead513e56dfce28
Collin Winner <cmwinner89@gmail.com>
2021-03-15 17:30:56 -0400

    Complete new_move_position

--- skeleton/lib/knight_travials.rb
+++ skeleton/lib/knight_travials.rb
@@ -2,7 +2,7 @@ require './00_tree_node.rb'
 
 class KnightPathFinder
     attr_reader :pos, :possible_moves
-    attr_accessor :root_node
+    attr_accessor :root_node, :considered_pos
 
     
     def self.valid_moves(pos)
@@ -34,30 +34,49 @@ class KnightPathFinder
         # build_move_tree
     end
 
-    # def new_move_positions(pos)
+    def new_move_positions(pos)
     #     #filter down valid_moves and exclude places we've been before
     #     #keeping track of places we've visited
     #     #use in build move tree
     #     #instance variable 
-    #     traveled = []
-    #     new_moves = []
-    #     @considered_pos << pos
-    #     traveled = considered_pos
-    #     @considered_pos = self.valid_moves(pos)
+        test_pos = KnightPathFinder.valid_moves(pos) 
+        test_pos.each do |ele|
+            @considered_pos << ele if !@considered_pos.include?(ele)
+        end
 
-    #     # self.valid_moves.each do |position|
-    #     #     if !considered_pos.include(position)
-    #     #         new_moves << position
-    #     #     end
-    #     # end
-    #     # @considered_pos = new_moves
-    # end 
+        @considered_pos
+        # @considered_pos.delete(pos)
+    end 
 
-    # def build_move_tree
+    def bfs(target = nil, &prc)
+        raise "Need a proc or target" if [target, prc].none?
+        prc ||= Proc.new {|node| node.value == target}
+
+        nodes = [self]
+        until nodes.empty?
+            node = nodes.shift
+
+            return node if prc.call(node)
+            nodes.concat(node.children)
+        end 
+        nil
+    end 
+
+    def build_move_tree
     #     # self.root_node = PolyTreeNode.new(pos)
     #     #use node class, each node represents position on board - value
     #     #construct from starting position every possible move
-    # end
+        
+        # self.root_node.bfs()
+        #create node for eahc pos on the board
+        #start with root node
+        #that nodes child = possible moves
+        #next nodes children + new_moves etc
+        root_node.value = pos
+        root_node.children = KnightPathFinder.valid_moves(pos)
+
+
+    end
 
 end
 
